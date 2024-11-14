b4fa823bdb5cd1084335e0327bed1f83aa3b000e
aaaa <aaaa@a.a>
2024-11-14 17:47:40 +0900

    Entity Exam

--- src/main/java/com/example/boardproj/entity/Exam.java
+++ src/main/java/com/example/boardproj/entity/Exam.java
@@ -1,4 +1,23 @@
 package com.example.boardproj.entity;
 
+import com.example.boardproj.entity.base.BaseEntity;
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Getter
+@Setter
+@ToString
+@AllArgsConstructor
+@NoArgsConstructor
+@Table
 public class Exam {
+
+        @Id
+        @GeneratedValue(strategy = GenerationType.IDENTITY)
+        private Long num;
+
+        private String name;
+
+        private String etc;
 }
